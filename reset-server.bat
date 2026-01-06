eb93ffb39b1eebbda9cf2e65f73e663c33236146
Marcus <marcusekidor@gmail.com>
2026-01-06 20:38:55 +0300

    Initial commit - KAPEYAMAHA ENTERPRISES website

--- reset-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Stopping all Node.js processes...
-taskkill /f /im node.exe 2>nul
-timeout /t 2 /nobreak >nul
-echo Starting KapeYamaha server on port 3001...
-set PORT=3001
-npm run dev
-pause
